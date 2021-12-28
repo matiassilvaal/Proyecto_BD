b1825020f4aa5481fa6eb481eed8c32ee5209e32
matiassilvaal <matias.silva.al@usach.cl>
2021-12-28 00:12:26 -0300

    [Matias Silva] - Migraciones 1
    Se agregaron migraciones

---
+++ database/migrations/2021_12_28_023530_create_requirements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequirementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requirements', function (Blueprint $table) {
+            $table->id();
+            $table->string("SO", 50);
+            $table->string("CPU", 50);
+            $table->integer("RAM");
+            $table->string("GPU", 100);
+            $table->integer("DirectX");
+            $table->string("RED", 100);
+            $table->integer("Uso de disco");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requirements');
+    }
+}
