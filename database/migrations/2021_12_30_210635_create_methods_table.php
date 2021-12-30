a664932d52ff686f2138b6d781208325697a4f06
Clemente <clemente.aguilar@usach.cl>
2021-12-30 18:23:55 -0300

    [Clemente Aguilar] - Migración de prueba

---
+++ database/migrations/2021_12_30_210635_create_methods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('methods', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_tarjeta')->nullable();
+            $table->foreign('id_tarjeta')->references('id')->on('cards');
+            $table->integer('numero');
+            $table->string('nombre', 200);
+            $table->date('fecha_de_vencimiento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('methods');
+    }
+}
