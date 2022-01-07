b83caf18718cdbad9532fbac72a2795a026bc361
matiassilvaal <matias.silva.al@usach.cl>
2022-01-07 01:20:25 -0300

    [Matias Silva] - Controladores
    Se agrego tambien el index y el show para la tabla Address

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Address;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $addresses = Address::all();
+        if($addresses->isEmpty()){
+            //No hay cursos
+            return response()->json([], 204);
+        }
+        return response($addresses, 200);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $address = Address::find($id);
+        if(empty($address)){
+            return response()->json([], 204);
+        }
+        return response($address, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/addresses','AddressController@index');
+Route::get('/address/{id}','AddressController@show');
